6dfbe0a2900f767ccce803ae730620a3f4293dcf
Shin-seung-hyun <jjack1028@naver.com>
2022-10-21 13:00:45 +0900

    first commit

---
+++ src/main/java/com/example/Unit2_TodoApp/Unit2TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit2_TodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2TodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Unit2_TodoApp/Unit2TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Unit2_TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
